19b789a57fca6f91d737acf2137e75db35f43192
ankit binjola <abinjola.binjola@gmail.com>
2020-04-14 21:31:20 +0530

    Create satta.js
    added js file and added set Interval function

---
+++ satta.js
@@ -0,0 +1,22 @@
+//satta interval for 2 hour 
+
+setInterval (function myFunction() {
+  var x = document.getElementById("demo")
+  var y = Math.floor((Math.random() * 2) + 1);
+switch (y) {
+
+    case 1:
+        var num1 = "1";
+        document.getElementById("demo").innerHTML=num1;
+        //console.log("1");
+        
+        break;
+
+        case 2:
+          var num2 = "6";
+          document.getElementById("demo").innerHTML=num2; 
+          //  console.log("6");
+            break;
+
+}
+},7200000);
